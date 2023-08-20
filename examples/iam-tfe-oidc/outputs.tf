b8e30e313d849a1749cdfbd3c4d8f2372d351003
Paweł Cholewa <pawel.cholewa@pcdev.pl>
2023-08-20 11:17:23 +0200

    Add outputs to example

--- examples/iam-tfe-oidc/outputs.tf
+++ examples/iam-tfe-oidc/outputs.tf
@@ -1,3 +1,11 @@
+output "identity_provider_url" {
+  value = module.iam-tfe-oidc.identity_provider_url
+}
+
+output "audience" {
+  value = module.iam-tfe-oidc.audience
+}
+
 output "role_arns" {
   value = module.iam-tfe-oidc.role_arns
 }
